4d866f71ccaedc934b5d023ad03381da16136801
Logfer <molnar.viktor999@gmail.com>
2022-10-12 10:35:25 +0200

    Rules

--- Amuba/Amuba/Form1.Designer.cs
+++ Amuba/Amuba/Form1.Designer.cs
@@ -29,13 +29,94 @@ namespace Amuba
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Rules = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Rules.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Rules
+            // 
+            this.Rules.Controls.Add(this.label4);
+            this.Rules.Controls.Add(this.label3);
+            this.Rules.Controls.Add(this.label2);
+            this.Rules.Location = new System.Drawing.Point(453, 216);
+            this.Rules.Name = "Rules";
+            this.Rules.Size = new System.Drawing.Size(200, 172);
+            this.Rules.TabIndex = 0;
+            this.Rules.TabStop = false;
+            this.Rules.Text = "Játékszabályok:";
+            this.Rules.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(6, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "O: X után lép";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(6, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "X: Ő kezdi a játékot";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(50, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Játékosok:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(753, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "?";
+            this.label1.MouseEnter += new System.EventHandler(this.label1_MouseEnter);
+            this.label1.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Rules);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Rules.ResumeLayout(false);
+            this.Rules.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox Rules;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Amuba/Amuba/Form1.cs
+++ Amuba/Amuba/Form1.cs
@@ -16,5 +16,32 @@ namespace Amuba
         {
             InitializeComponent();
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://hu.wikipedia.org/wiki/Am%C5%91ba_(j%C3%A1t%C3%A9k)");
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+            Rules.Visible = true;
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            Rules.Visible = false;
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Rules.Location = new Point(RelMousPoz().X - Rules.Width, RelMousPoz().Y);
+            Rules.BringToFront();
+        }
+        private Point RelMousPoz()
+        {
+            return new Point(MousePosition.X - this.Location.X - 8, MousePosition.Y - this.Location.Y - 30);
+        }
+
     }
 }
+
