463c598efd14c3b113d78c5204324e9878eb4261
Logfer <molnar.viktor999@gmail.com>
2022-10-14 13:03:44 +0200

    Rules completed

--- Amuba/Amuba/Form1.Designer.cs
+++ Amuba/Amuba/Form1.Designer.cs
@@ -30,55 +30,21 @@ namespace Amuba
         private void InitializeComponent()
         {
             this.Rules = new System.Windows.Forms.GroupBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.Leiras = new System.Windows.Forms.Label();
             this.Rules.SuspendLayout();
             this.SuspendLayout();
             // 
             // Rules
             // 
-            this.Rules.Controls.Add(this.label4);
-            this.Rules.Controls.Add(this.label3);
-            this.Rules.Controls.Add(this.label2);
-            this.Rules.Location = new System.Drawing.Point(453, 216);
+            this.Rules.Controls.Add(this.Leiras);
+            this.Rules.Location = new System.Drawing.Point(537, 21);
             this.Rules.Name = "Rules";
-            this.Rules.Size = new System.Drawing.Size(200, 172);
+            this.Rules.Size = new System.Drawing.Size(200, 149);
             this.Rules.TabIndex = 0;
             this.Rules.TabStop = false;
             this.Rules.Text = "Játékszabályok:";
             this.Rules.Visible = false;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(6, 80);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(77, 15);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "O: X után lép";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(6, 56);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(111, 15);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "X: Ő kezdi a játékot";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(50, 26);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 18);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Játékosok:";
             // 
             // label1
             // 
@@ -94,6 +60,16 @@ namespace Amuba
             this.label1.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
             this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
             // 
+            // Leiras
+            // 
+            this.Leiras.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Leiras.Location = new System.Drawing.Point(6, 16);
+            this.Leiras.Name = "Leiras";
+            this.Leiras.Size = new System.Drawing.Size(188, 129);
+            this.Leiras.TabIndex = 0;
+            this.Leiras.Text = "Két játékos játszik egy 10x10-es pályán.\r\n\r\nA cél kirakni 5 egybefüggő szimbólumo" +
+    "t.\r\n\r\namelyik játékos ezt előbb eléri az nyer.\r\n";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -104,7 +80,6 @@ namespace Amuba
             this.Name = "Form1";
             this.Text = "Form1";
             this.Rules.ResumeLayout(false);
-            this.Rules.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,9 +89,7 @@ namespace Amuba
 
         private System.Windows.Forms.GroupBox Rules;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Leiras;
     }
 }
 
